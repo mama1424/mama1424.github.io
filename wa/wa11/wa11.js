8b792319445e2cc9e7689608056d64cec9ccd226
mama1424 <130328583+mama1424@users.noreply.github.com>
2024-11-08 10:02:32 -0700

    wa11

---
+++ wa/wa11/wa11.js
@@ -0,0 +1,27 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+let imageArray = ['/images/pic1.jpg','/images/pic2.jpg', '/images/pic3.jpg','/images/pic4.jpg','/images/pic5.jpg']
+
+
+/* Declaring the alternative text for each image file */
+
+
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
+btn.setAttribute("class", xxx);
+btn.textContent = xxx;
+overlay.style.backgroundColor = xxx;
+
+btn.getAttribute();
