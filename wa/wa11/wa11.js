01d31773849baeac24f16719b69c28bc0c6c4e45
mama1424 <130328583+mama1424@users.noreply.github.com>
2024-11-12 21:28:27 -0700

    Update wa11.js

--- wa/wa11/wa11.js
+++ wa/wa11/wa11.js
@@ -5,12 +5,12 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-let imageArray = ['/images/pic1.jpg','/images/pic2.jpg', '/images/pic3.jpg','/images/pic4.jpg','/images/pic5.jpg']
-
+const images = ['/images/pic1.jpg','/images/pic2.jpg', '/images/pic3.jpg','/images/pic4.jpg','/images/pic5.jpg']
 
 /* Declaring the alternative text for each image file */
-
-
+const altTextImg = {
+    '/images/pic1.jpg' : 'Image of eye',
+}
 
 /* Looping through images */
 
@@ -19,9 +19,31 @@ newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
-/* Wiring up the Darken/Lighten button */
-btn.setAttribute("class", xxx);
-btn.textContent = xxx;
-overlay.style.backgroundColor = xxx;
+images.forEach(image =>{ const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', altText[image]);
+    thumbBar.appendChild(newImage);  
+    }
+)
 
-btn.getAttribute();
+newImage.addEventListener('click', () => {
+    displayedImage.setAttribute('src', newImage.getAttribute('src'));
+    displayedImage.setAttribute('alt', newImage.getAttribute('alt'));
+  }
+);
+
+
+/* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    const currentClass = btn.getAttribute('class');
+  
+    if (currentClass === 'dark') {
+      btn.setAttribute('class', 'light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    } else {
+      btn.setAttribute('class', 'dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    }
+  });
